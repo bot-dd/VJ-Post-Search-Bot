cbaa0da02b0dfa0c8a8ce11d012d873c99c6f5a7
bot-dd <botdd445@gmail.com>
2025-06-02 11:24:30 +0600

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "26649585"))
+API_HASH     = environ.get("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002312610528"))
+ADMIN        = int(environ.get("ADMIN", "7822720438"))
+CHANNEL      = environ.get("CHANNEL", "@RM_Movie_Flix")
